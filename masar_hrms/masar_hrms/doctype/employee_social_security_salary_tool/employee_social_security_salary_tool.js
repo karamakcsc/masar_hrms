fe5608a6aee6497ae0013fd91e93273eede31edc
karamakcsc <karamakcsc@hotmail.com>
2023-01-22 08:50:33 +0000

    First Upload

---
+++ masar_hrms/masar_hrms/doctype/employee_social_security_salary_tool/employee_social_security_salary_tool.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, KCSC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Social Security Salary Tool', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ masar_hrms/masar_hrms/doctype/overtime_type/test_overtime_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, KCSC and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestOvertimeType(unittest.TestCase):
+	pass
