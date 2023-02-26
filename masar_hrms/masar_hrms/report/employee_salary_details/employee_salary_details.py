a532060a025a8f8b0b5fd158cf44a39d3d3114d1
karamakcsc <karamakcsc@hotmail.com>
2023-02-26 14:39:49 +0300

    Employee Salary Details

--- masar_hrms/masar_hrms/report/employee_salary_details/employee_salary_details.py
+++ masar_hrms/masar_hrms/report/employee_salary_details/employee_salary_details.py
@@ -27,29 +27,29 @@ def get_data(filters):
 	data = frappe.db.sql(f"""SELECT tss.name AS `Salary Slip No.`, tss.employee AS `Employee No.`,
 									tss.employee_name AS `Employee Name`, tss.branch AS `Branch`,
 									tss.department AS `Department`, tss.designation AS `Designation`, te.date_of_joining AS `Date of Joining`,tssa.base AS `Basic Salary`,
-									tss.gross_pay AS `Total Earnings`,
 									MAX(CASE WHEN tsd.salary_component = 'Overtime Allowance' THEN tsd.amount END) AS `Overtime Allowance`,
 									MAX(CASE WHEN tsd.salary_component = 'Awards IN __ OUT'AND tsd.salary_component = 'Non Taxable Bonus' AND tsd.salary_component = 'End Service Awards' AND tsd.salary_component = 'Project Awards' AND tsd.salary_component = 'Award' AND tsd.salary_component = 'Bonus IN-OUT' THEN tsd.amount END) AS `Awards`,
 									(SELECT SUM(IF(tsd.salary_component != 'Overtime Allowance' AND tsd.salary_component != 'Basic' AND tsd.salary_component = 'Awards IN __ OUT'AND tsd.salary_component = 'Non Taxable Bonus' AND tsd.salary_component = 'End Service Awards' AND tsd.salary_component = 'Project Awards' AND tsd.salary_component = 'Award' AND tsd.salary_component = 'Bonus IN-OUT', tsd.amount, 0))
 									FROM `tabSalary Detail` tsd
-        							WHERE tsd.parent = tss.name AND tsd.parentfield = 'earnings') AS `Other Earnings`,
-									tss.total_deduction AS `Total Deductions`,
-       								MAX(CASE WHEN tsd.salary_component = 'Social Security' THEN tsd.amount END) AS `Social Security`,
-       								MAX(CASE WHEN tsd.salary_component = 'Income Tax' THEN tsd.amount END) AS `Income Tax`,
-       								MAX(CASE WHEN tss.total_loan_repayment > 0 THEN tss.total_loan_repayment ELSE 0 END) AS `Loan`,
-       								(SELECT SUM(IF(tsd.salary_component != 'Income Tax' AND tsd.salary_component != 'Social Security', tsd.amount, 0))
-        					FROM `tabSalary Detail` tsd
-        					WHERE tsd.parent = tss.name AND tsd.parentfield = 'deductions') AS `Other Deductions`, tss.net_pay AS `Net Pay`, tss.posting_date
+									WHERE tsd.parent = tss.name AND tsd.parentfield = 'earnings') AS `Other Earnings`,
+									tss.gross_pay AS `Total Earnings`,
+									MAX(CASE WHEN tsd.salary_component = 'Social Security' THEN tsd.amount END) AS `Social Security`,
+									MAX(CASE WHEN tsd.salary_component = 'Income Tax' THEN tsd.amount END) AS `Income Tax`,
+									MAX(CASE WHEN tss.total_loan_repayment > 0 THEN tss.total_loan_repayment ELSE 0 END) AS `Loan`,
+									(SELECT SUM(IF(tsd.salary_component != 'Income Tax' AND tsd.salary_component != 'Social Security', tsd.amount, 0))
+							FROM `tabSalary Detail` tsd
+							WHERE tsd.parent = tss.name AND tsd.parentfield = 'deductions') AS `Other Deductions`, tss.total_deduction AS `Total Deductions`,
+							tss.net_pay AS `Net Pay`, tss.posting_date
 							FROM `tabSalary Slip` tss
 							INNER JOIN `tabSalary Detail` tsd ON tss.name = tsd.parent
 							INNER JOIN `tabSalary Structure Assignment` tssa ON tssa.employee = tss.employee
 							INNER JOIN `tabEmployee` te ON te.name = tss.employee
 							INNER JOIN `tabSalary Slip` tss_sub ON tss_sub.name = tss.name
 							WHERE tss.docstatus = 1 AND  tss_sub.name = tss.name
- 										And (tss.posting_date BETWEEN '{_from}' AND '{to}')
-							 			{conditions}GROUP BY tss.name, tss.employee, tss.employee_name, tss.department,
-							 			tss.designation, te.date_of_joining, tssa.base, tss.gross_pay, tss.payment_days,
-							 			tss.total_deduction, tss.net_pay ;""")
+										And (tss.posting_date BETWEEN '{_from}' AND '{to}')
+										{conditions}GROUP BY tss.name, tss.employee, tss.employee_name, tss.department,
+										tss.designation, te.date_of_joining, tssa.base, tss.gross_pay, tss.payment_days,
+										tss.total_deduction, tss.net_pay ;""")
 
 	return data
 
@@ -63,16 +63,16 @@ def get_columns():
 	   "Designation: Data:200",
 	   "Date of Joining: Data:200 ",
 	   "Basic Salary: Data:200",
-	   "Total Earnings: Data:200",
 	   "Overtime Allowance: Data:200",
 	   "Awards: Data:200",
 	   "Other Earnings: Data:200",
+	   "Total Earnings: Data:200",
 	   #"Working Days: Data:200",
-	   "Total Deductions: Data:200",
 	   "Social Security: Data:200",
 	   "Income Tax: Data:200",
 	   "Loan: Data:200",
 	   "Other Deductions: Data:200",
+	   "Total Deductions: Data:200",
 	   "Net Pay: Data:200"
 	   #"Posting Date: Date/Posting Date:150"
 	   # "Tax Group: Data:200",
