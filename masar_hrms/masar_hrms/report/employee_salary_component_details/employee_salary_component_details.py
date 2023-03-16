2a95b6f6375fc309b4931feed03badedf40f9f43
karamakcsc <karamakcsc@hotmail.com>
2023-03-16 14:08:40 +0300

    overtime

--- masar_hrms/masar_hrms/report/employee_salary_component_details/employee_salary_component_details.py
+++ masar_hrms/masar_hrms/report/employee_salary_component_details/employee_salary_component_details.py
@@ -23,69 +23,8 @@ def get_data(filters):
 	if(filters.get('branch')):conditions += f" AND te.branch LIKE '%{filters.get('branch')}' "
 	if(filters.get('dep')):conditions += f" AND te.department LIKE '%{filters.get('dep')}' "
 
-	#SQL Query
-	# data = frappe.db.sql(f"""SELECT tss.employee AS `Employee No.`,
-	# 								tss.employee_name AS `Employee Name`, tss.branch AS `Branch`, te.work_type AS `Work Type`, tss.company AS `Company`,
-	# 								tss.department AS `Department`, tss.designation AS `Designation`, te.date_of_joining AS `Date of Joining`,
-	# 								tssa.base AS `Basic Salary`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Transportation' THEN tsd.amount END) AS `Transportation`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Expatriate Allowance' THEN tsd.amount END) AS `Expatriate Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Mobile Allowance' THEN tsd.amount END) AS `Mobile Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Overtime Allowance' THEN tsd.amount END) AS `Overtime Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Field Allowance' THEN tsd.amount END) AS `Field Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Housing' THEN tsd.amount END) AS `Housing`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Work days allowance' THEN tsd.amount END) AS `Work Days Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Project Awards' THEN tsd.amount END) AS `Project Awards`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Fixed Allowance' THEN tsd.amount END) AS `Fixed Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Fuel Allowance' THEN tsd.amount END) AS `Fuel Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Fund Allowance' THEN tsd.amount END) AS `Fund Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Master Degree Allowance' THEN tsd.amount END) AS `Master Degree Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Board Allowance' THEN tsd.amount END) AS `Board Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Children Education Allowance' THEN tsd.amount END) AS `Children Education Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'End Service Awards' THEN tsd.amount END) AS `End Service Awards`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Award' THEN tsd.amount END) AS `Award`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Arrear' THEN tsd.amount END) AS `Arrear`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Leave Encashment' THEN tsd.amount END) AS `Leave Encashment`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Medical Treatment Allowance' THEN tsd.amount END) AS `Medical Treatment Allowance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Non Taxable Bonus' THEN tsd.amount END) AS `Non Taxable Bonus`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Notice Period' THEN tsd.amount END) AS `Notice Period`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Refunds' THEN tsd.amount END) AS `Refunds`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Salaries Differences - Earning' THEN tsd.amount END) AS `Salaries Differences-Earning`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Social Security Difference' THEN tsd.amount END) AS `Social Security Difference`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Awards IN __ OUT' THEN tsd.amount END) AS `Awards IN-OUT`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Vacation allowance - current year' THEN tsd.amount END) AS `Vacation Allowance-Current Year`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Vacation allowance - past year' THEN tsd.amount END) AS `Vacation Allowance-Past Year`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Social Security' THEN tsd.amount END) AS `Social Security`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Income Tax' THEN tsd.amount END) AS `Income Tax`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Health  Insurance' THEN tsd.amount END) AS `Health  Insurance`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Health Insurance Fees' THEN tsd.amount END) AS `Health Insurance Fees`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Hussein Cancer Center Donation' THEN tsd.amount END) AS `Hussein Cancer Center Donation`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Bonus IN-OUT' THEN tsd.amount END) AS `Bonus IN-OUT`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Catering Deduction' THEN tsd.amount END) AS `Catering Deduction`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Jordan Engineers Association subscriptions and loans' THEN tsd.amount END) AS `Jordan Engineers Association Subscriptions and loans`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Penalty as per Jordan Labor Law' THEN tsd.amount END) AS `Penalty as per Jordan Labor Law`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Penalty Internal Law' THEN tsd.amount END) AS `Penalty Internal Law`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Safety Violation' THEN tsd.amount END) AS `Safety Violation`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Salary Differences - Deduction' THEN tsd.amount END) AS `Salary Differences-Deduction`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Traffic Violation' THEN tsd.amount END) AS `Traffic Violation`,
-	# 								MAX(CASE WHEN tsd.salary_component = 'Warehouse custody allowance' THEN tsd.amount END) AS `Warehouse Custody Allowance`,
-	# 								MAX(CASE WHEN tss.total_loan_repayment > 0 THEN tss.total_loan_repayment ELSE 0 END) AS `Loan`
-									
-	# 						FROM `tabSalary Slip` tss
-	# 						INNER JOIN `tabSalary Detail` tsd ON tss.name = tsd.parent
-	# 						INNER JOIN `tabSalary Structure Assignment` tssa ON tssa.employee = tss.employee
-	# 						INNER JOIN `tabEmployee` te ON te.name = tss.employee
-	# 						INNER JOIN `tabSalary Slip` tss_sub ON tss_sub.name = tss.name
-	# 						WHERE tss.docstatus = 1 AND  tss_sub.name = tss.name
-	# 									And (tss.posting_date BETWEEN '{_from}' AND '{to}')
-	# 									{conditions}
-	# 									GROUP BY tss.name, tss.employee, tss.employee_name, tss.department,
-	# 									tss.designation, te.date_of_joining, tssa.base;""")
-
-	# return data
-
 	data = frappe.db.sql(f"""select te.name AS `Employee No.`, te.employee_name AS `Employee Name`, te.date_of_joining AS `Date of Joining`, te.work_type AS `Work Type`, tss.company AS `Company`,
-									te.branch AS `Branch`, te.department AS `Department`, te.designation AS `Designation`, 
+									te.branch AS `Branch`, te.department AS `Department`, te.designation AS `Designation`,
 									tssa.base AS `Basic Salary`,
 									MAX(CASE WHEN tsd.salary_component = 'Transportation' THEN tsd.amount END) AS `Transportation`,
 									MAX(CASE WHEN tsd.salary_component = 'Expatriate Allowance' THEN tsd.amount END) AS `Expatriate Allowance`,
@@ -130,32 +69,17 @@ def get_data(filters):
 									MAX(CASE WHEN tsd.salary_component = 'Traffic Violation' THEN tsd.amount END) AS `Traffic Violation`,
 									MAX(CASE WHEN tsd.salary_component = 'Warehouse custody allowance' THEN tsd.amount END) AS `Warehouse cCustody Allowance`
 									-- MAX(CASE WHEN tss.total_loan_repayment > 0 THEN tss.total_loan_repayment ELSE 0 END) AS `Loan`
-									from `tabSalary Structure` tss 
-									inner join `tabSalary Structure Assignment` tssa on tss.name = tssa.salary_structure 
+									from `tabSalary Structure` tss
+									inner join `tabSalary Structure Assignment` tssa on tss.name = tssa.salary_structure
 									INNER join `tabSalary Detail` tsd on tsd.parent  = tss.name
 									inner join `tabSalary Component` tsc on tsd.salary_component = tsc.name
-									inner join `tabEmployee` te on tssa.employee = te.name 
-									INNER JOIN `tabEmployee Social Security Salary` tesss on te.name = tesss.employee 
+									inner join `tabEmployee` te on tssa.employee = te.name
+									INNER JOIN `tabEmployee Social Security Salary` tesss on te.name = tesss.employee
 								WHERE tssa.docstatus = 1
 										{conditions}
 								GROUP BY tss.name, tssa.employee, tssa.base, te.social_security_amount;""")
 
 	return data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def get_columns():
 	return [
@@ -211,6 +135,6 @@ def get_columns():
 	   "Traffic Violation: Data:200",
 	   "Warehouse Custody Allowance: Data:200"
 	#    "Loan: Data:200"
-	   
+
 
 	]
